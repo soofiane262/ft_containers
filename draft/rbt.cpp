2bf206d0422c03d212d1b3f85afd4e0d2774c51c
Soufiane El marsi <sel-mars@e2r3p6.1337.ma>
2023-02-03 22:22:20 +0100

    rbt-insert-ok

--- draft/rbt.cpp
+++ draft/rbt.cpp
@@ -6,7 +6,7 @@
 /*   By: sel-mars <sel-mars@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 17:57:44 by sel-mars          #+#    #+#             */
-/*   Updated: 2023/02/01 19:38:54 by sel-mars         ###   ########.fr       */
+/*   Updated: 2023/02/03 22:21:52 by sel-mars         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,14 @@ int main( void ) {
 	std::system( "clear" );
 	{
 		rbt< int > n;
-		n.insert( 4 );
 		n.insert( 3 );
+		n.insert( 4 );
 		n.insert( 5 );
-		// n.root->_right->recolour();
-		n.log();
-		n.insert( 2 );
+		// n.insert( 3 );
+		// n.insert( 2 );
+		// n.insert( 4 );
+		// n.insert( 5 );
+		// n.insert( 2 );
 		// n.del( 6 );
 		// n.del( 4 );
 		// n.del( 2 );
--- draft/rbt.hpp
+++ draft/rbt.hpp
@@ -6,7 +6,7 @@
 /*   By: sel-mars <sel-mars@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 17:55:21 by sel-mars          #+#    #+#             */
-/*   Updated: 2023/02/01 19:38:14 by sel-mars         ###   ########.fr       */
+/*   Updated: 2023/02/03 22:21:51 by sel-mars         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,18 @@
 
 #define BLACK_NODE true
 #define RED_NODE   false
+#define LEFT_ROT   1
+#define RIGHT_ROT  2
 
 template < class T > struct node {
 	T		   _elt;
 	bool	   _color;
+	int		   _change;
 	node< T > *_parent, *_left, *_right;
 	node( const T &elt, node< T > *parent ) {
 		_elt	= elt;
 		_color	= RED_NODE;
+		_change = 0;
 		_left	= NULL;
 		_right	= NULL;
 		_parent = parent;
@@ -34,25 +38,36 @@ template < class T > struct node {
 
 template < class T > class rbt {
   private:
-	node< T > *getGrandfather( node< T > *nd ) {
-		return nd->_parent ? nd->_parent->_parent : NULL;
+	node< T > *getSibling( node< T > *nd ) {
+		if ( !nd->_parent ) return NULL;
+		else if ( nd == nd->_parent->_left )
+			return nd->_parent->_right;
+		return nd->_parent->_left;
 	};
-	node< T > *getUncle( node< T > *nd ) {
-		node< T > *grandFather = getGrandfather( nd );
-		if ( nd->_parent == grandFather->_left ) return grandFather->_right;
-		return grandFather->_left;
+	void swapColors( node< T > *&x, node< T > *&y ) {
+		bool tmpColor = x->_color;
+		x->_color	  = y->_color;
+		y->_color	  = tmpColor;
 	};
 	void leftRotation( node< T > *&nd ) {
-		struct node< T > *r = nd->_right, *rl = r->_left;
-		r->_left   = nd;
-		nd->_right = rl;
-		nd		   = r;
+		node< T > *init = nd;
+		node< T > *r = nd->_right, *rl = r->_left;
+		r->_left	  = nd;
+		nd->_right	  = rl;
+		nd			  = r;
+		nd->_parent	  = init->_parent;
+		init->_parent = nd;
+		if ( rl ) rl->_parent = init;
 	};
 	void rightRotation( node< T > *&nd ) {
-		struct node< T > *l = nd->_left, *lr = l->_right;
-		l->_right = nd;
-		nd->_left = lr;
-		nd		  = l;
+		node< T > *init = nd;
+		node< T > *l = nd->_left, *lr = l->_right;
+		l->_right	  = nd;
+		nd->_left	  = lr;
+		nd			  = l;
+		nd->_parent	  = init->_parent;
+		init->_parent = nd;
+		if ( lr ) lr->_parent = init;
 	};
 	bool find( node< T > &nd, const T &elt ) {
 		if ( elt == nd._elt ) return true;
@@ -67,34 +82,47 @@ template < class T > class rbt {
 		}
 	};
 	void _insert( node< T > *&nd, const T &elt, node< T > *parent ) {
-		if ( !nd ) nd = new node< T >( elt, parent );
-		else if ( elt < nd->_elt )
+		if ( !nd ) {
+			nd = new node< T >( elt, parent );
+			return;
+		} else if ( elt < nd->_elt )
 			_insert( nd->_left, elt, nd );
 		else if ( elt > nd->_elt )
 			_insert( nd->_right, elt, nd );
 		else
 			return;
-		node< T > *grandfather = getGrandfather( nd );
-		if ( !nd->_parent ) nd->_color = BLACK_NODE;
-		if ( !grandfather ) return;
-		std::cout << "i am " << nd->_elt << "\nmy parent is " << ( parent ? parent->_elt : NULL )
-				  << "\n";
-		node< T > *uncle = getUncle( nd );
-		std::cout << "my grandfather is " << ( grandfather ? grandfather->_elt : NULL )
-				  << "\nand my uncle is " << ( uncle ? uncle->_elt : NULL ) << '\n';
-		if ( nd->_parent->_color == RED_NODE && uncle ) {
-			if ( uncle->_color == RED_NODE ) {
-				nd->_parent->_color = BLACK_NODE;
-				uncle->_color		= BLACK_NODE;
-				grandfather->_color = RED_NODE;
+		switch ( nd->_change ) {
+			case RIGHT_ROT:
+				nd->_change = 0;
+				rightRotation( nd );
+				swapColors( nd, nd->_right );
+				return;
+			case LEFT_ROT:
+				nd->_change = 0;
+				leftRotation( nd );
+				swapColors( nd, nd->_left );
+				return;
+		}
+		node< T > *sibling = getSibling( nd );
+		if ( !nd->_parent || nd->_color == BLACK_NODE ||
+			 ( ( !nd->_left || nd->_left->_color == BLACK_NODE ) &&
+			   ( !nd->_right || nd->_right->_color == BLACK_NODE ) ) )
+			return;
+		else if ( !sibling || sibling->_color == BLACK_NODE ) {
+			if ( nd == nd->_parent->_left && elt < nd->_elt ) nd->_parent->_change = RIGHT_ROT;
+			else if ( nd == nd->_parent->_right && elt > nd->_elt )
+				nd->_parent->_change = LEFT_ROT;
+			else if ( nd == nd->_parent->_left && elt > nd->_elt ) {
+				leftRotation( nd );
+				nd->_parent->_change = RIGHT_ROT;
 			} else {
-				// if ( grandfather && nd == grandfather->_left->_left ) {
-				// 	rightRotation( grandfather );
-				// 	bool tmpColor				= nd->_parent->_color;
-				// 	nd->_parent->_color			= nd->_parent->_right->_color;
-				// 	nd->_parent->_right->_color = tmpColor;
-				// }
+				rightRotation( nd );
+				nd->_parent->_change = LEFT_ROT;
 			}
+		} else {
+			nd->_color			= BLACK_NODE;
+			sibling->_color		= BLACK_NODE;
+			nd->_parent->_color = RED_NODE;
 		}
 	};
 	void destruct( node< T > *&nd ) {
@@ -110,17 +138,15 @@ template < class T > class rbt {
 		std::cout << ( x->_color == BLACK_NODE ? "\e[1;37m" : "\e[1;31m" ) << x->_elt << "\e[0m\n";
 		_log( x->_left, lvl + 1 );
 	};
+	node< T > *root;
 
   public:
-	node< T > *root;
 	rbt( void ) { root = NULL; };
 	rbt( const T &n ) { root = new node< T >( n ); };
 	~rbt( void ) { destruct( root ); };
 	void insert( const T &elt ) {
 		_insert( root, elt, NULL );
-		// temporary
-		log();
-		//
+		root->_color = BLACK_NODE;
 	};
 	// bool find( const T &elt ) {
 	// 	if ( !root ) return false;
