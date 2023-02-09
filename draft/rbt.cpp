5a900ef953ac176d651fdb206024e771da9daf34
Soufiane El marsi <sel-mars@e2r4p10.1337.ma>
2023-02-09 19:31:23 +0100

    rbt-del-wip

--- draft/rbt.cpp
+++ draft/rbt.cpp
@@ -6,31 +6,127 @@
 /*   By: sel-mars <sel-mars@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 17:57:44 by sel-mars          #+#    #+#             */
-/*   Updated: 2023/02/03 22:21:52 by sel-mars         ###   ########.fr       */
+/*   Updated: 2023/02/09 19:27:31 by sel-mars         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rbt.hpp"
 
 #include <cstdlib>
+#include <ctime>
+#include <ios>
 #include <vector>
 
 int main( void ) {
 	std::system( "clear" );
+	std::cout << std::boolalpha;
+	srand( time( NULL ) );
+
+	// random
 	{
+		int				   sz = 20;
+		std::vector< int > vec;
+		for ( int i = 0; i < sz; i++ ) vec.push_back( rand() % 100 );
 		rbt< int > n;
-		n.insert( 3 );
-		n.insert( 4 );
-		n.insert( 5 );
-		// n.insert( 3 );
-		// n.insert( 2 );
-		// n.insert( 4 );
-		// n.insert( 5 );
-		// n.insert( 2 );
-		// n.del( 6 );
-		// n.del( 4 );
-		// n.del( 2 );
+		for ( int i = 0; i < sz; i++ ) {
+			std::cout << "insert " << vec[ i ] << '\n';
+			// while ( std::getchar() != '\n' ) continue;
+			n.insert( vec[ i ] );
+			n.log();
+			// while ( std::getchar() != '\n' ) continue;
+		}
+		for ( int i = 0, j; i < sz / 2; i++ ) {
+			j = rand() % vec.size();
+			std::cout << "delete " << vec[ j ] << '\n';
+			// while ( std::getchar() != '\n' ) continue;
+			n.del( vec[ j ] );
+			n.log();
+			// while ( std::getchar() != '\n' ) continue;
+		}
+		n.log();
 	}
+
+	// simple test
+	// {
+	// 	rbt< int > n;
+	// 	for ( int i = 1; i < 10; i++ ) n.insert( i );
+	// 	n.log();
+	// 	std::cout << "find 0\t: " << n.find( 0 ) << '\n';
+	// 	std::cout << "find 1\t: " << n.find( 1 ) << '\n';
+	// 	std::cout << "find 9\t: " << n.find( 9 ) << '\n';
+	// 	std::cout << "find 10\t: " << n.find( 10 ) << '\n';
+	// 	n.del( 5 );
+	// 	n.log();
+	// }
+
+	// case 1 --> if ( elt != 5 && elt != 30 && elt != 38 ) _color = BLACK; ==> OK
+	// {
+	// 	rbt< int > n;
+	// 	n.insert( 10 );
+	// 	n.insert( 5 );
+	// 	n.insert( 30 );
+	// 	n.insert( 2 );
+	// 	n.insert( 9 );
+	// 	n.insert( 25 );
+	// 	n.insert( 40 );
+	// 	n.insert( 38 );
+	// 	n.del( 30 );
+	// }
+
+	// case 3 --> if ( elt != 20 ) _color = BLACK; ==> OK
+	// {
+	// 	rbt< int > n;
+	// 	n.insert( 10 );
+	// 	n.insert( 5 );
+	// 	n.insert( 20 );
+	// 	n.insert( 15 );
+	// 	n.insert( 30 );
+	// 	n.del( 15 );
+	// }
+
+	// cases 3 & 2 --> _color = BLACK; ==> OK
+	// {
+	// 	rbt< int > n;
+	// 	n.insert( 10 );
+	// 	n.insert( 5 );
+	// 	n.insert( 20 );
+	// 	n.insert( 1 );
+	// 	n.insert( 7 );
+	// 	n.insert( 15 );
+	// 	n.insert( 30 );
+	// 	n.del( 15 );
+	// }
+
+	// case 4 --> if ( elt != 30 ) _color = BLACK; ==> OK
+	// {
+	// 	rbt< int > n;
+	// 	n.insert( 10 );
+	// 	n.insert( 5 );
+	// 	n.insert( 20 );
+	// 	n.insert( 1 );
+	// 	n.insert( 7 );
+	// 	n.insert( 15 );
+	// 	n.insert( 30 );
+	// 	n.insert( 25 );
+	// 	n.insert( 40 );
+	// 	n.del( 15 );
+	// }
+
+	// cases 5 & 6 --> if ( elt != 25 ) _color = BLACK; ==> OK
+	// {
+	// 	rbt< int > n;
+	// 	n.insert( 10 );
+	// 	n.insert( 5 );
+	// 	n.insert( 30 );
+	// 	n.insert( 1 );
+	// 	n.insert( 7 );
+	// 	n.insert( 25 );
+	// 	n.insert( 40 );
+	// 	n.insert( 20 );
+	// 	n.insert( 28 );
+	// 	n.del( 1 );
+	// }
+
 	// std::system( "leaks a.out" );
 
 	return 0;
--- draft/rbt.hpp
+++ draft/rbt.hpp
@@ -6,52 +6,66 @@
 /*   By: sel-mars <sel-mars@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 17:55:21 by sel-mars          #+#    #+#             */
-/*   Updated: 2023/02/03 22:21:51 by sel-mars         ###   ########.fr       */
+/*   Updated: 2023/02/09 19:29:24 by sel-mars         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
 #include <algorithm>
+#include <cstdlib>
 #include <iostream>
 
-#define BLACK_NODE true
-#define RED_NODE   false
-#define LEFT_ROT   1
-#define RIGHT_ROT  2
+// tmp
+#include <unistd.h>
+//
+
+#define RED			 0
+#define BLACK		 1
+#define DOUBLE_BLACK 2
+#define LEFT		 1
+#define RIGHT		 2
 
 template < class T > struct node {
 	T		   _elt;
-	bool	   _color;
-	int		   _change;
+	int		   _color;
+	int		   _rotate;
+	int		   _nil;
 	node< T > *_parent, *_left, *_right;
 	node( const T &elt, node< T > *parent ) {
 		_elt	= elt;
-		_color	= RED_NODE;
-		_change = 0;
+		_color	= RED;
+		_rotate = 0;
+		_nil	= 0;
 		_left	= NULL;
 		_right	= NULL;
 		_parent = parent;
 	};
-	void recolour( void ) { _color = !_color; };
 };
 
 template < class T > class rbt {
   private:
-	node< T > *getSibling( node< T > *nd ) {
+	typedef node< T > node;
+	node			 *getSibling( node *nd ) {
 		if ( !nd->_parent ) return NULL;
 		else if ( nd == nd->_parent->_left )
 			return nd->_parent->_right;
 		return nd->_parent->_left;
 	};
-	void swapColors( node< T > *&x, node< T > *&y ) {
+	node *getDBSibling( node *nd ) {
+		if ( nd->_color == DOUBLE_BLACK ) return getSibling( nd );
+		else if ( nd->_nil == LEFT )
+			return nd->_right;
+		return nd->_left;
+	};
+	void swapColors( node *&x, node *&y ) {
 		bool tmpColor = x->_color;
 		x->_color	  = y->_color;
 		y->_color	  = tmpColor;
 	};
-	void leftRotation( node< T > *&nd ) {
-		node< T > *init = nd;
-		node< T > *r = nd->_right, *rl = r->_left;
+	void leftRotation( node *&nd ) {
+		node *init = nd;
+		node *r = nd->_right, *rl = r->_left;
 		r->_left	  = nd;
 		nd->_right	  = rl;
 		nd			  = r;
@@ -59,9 +73,9 @@ template < class T > class rbt {
 		init->_parent = nd;
 		if ( rl ) rl->_parent = init;
 	};
-	void rightRotation( node< T > *&nd ) {
-		node< T > *init = nd;
-		node< T > *l = nd->_left, *lr = l->_right;
+	void rightRotation( node *&nd ) {
+		node *init = nd;
+		node *l = nd->_left, *lr = l->_right;
 		l->_right	  = nd;
 		nd->_left	  = lr;
 		nd			  = l;
@@ -69,21 +83,35 @@ template < class T > class rbt {
 		init->_parent = nd;
 		if ( lr ) lr->_parent = init;
 	};
-	bool find( node< T > &nd, const T &elt ) {
-		if ( elt == nd._elt ) return true;
-		else if ( elt < nd._elt ) {
-			if ( !nd.left ) return false;
-			else
-				return find( *( nd.left ), elt );
-		} else {
-			if ( !nd.right ) return false;
-			else
-				return find( *( nd.right ), elt );
+	void rotate( node *&nd, const bool colorSwap ) {
+		switch ( nd->_rotate ) {
+			case RIGHT:
+				nd->_rotate = 0;
+				rightRotation( nd );
+				if ( colorSwap ) swapColors( nd, nd->_right );
+				return;
+			case LEFT:
+				nd->_rotate = 0;
+				leftRotation( nd );
+				if ( colorSwap ) swapColors( nd, nd->_left );
+				return;
 		}
 	};
-	void _insert( node< T > *&nd, const T &elt, node< T > *parent ) {
+	bool find( node &nd, const T &elt ) {
+		if ( elt == nd._elt ) return true;
+		else if ( elt < nd._elt ) {
+			if ( !nd._left ) return false;
+			else
+				return find( *( nd._left ), elt );
+		} else {
+			if ( !nd._right ) return false;
+			else
+				return find( *( nd._right ), elt );
+		}
+	};
+	void _insert( node *&nd, const T &elt, node *parent ) {
 		if ( !nd ) {
-			nd = new node< T >( elt, parent );
+			nd = new node( elt, parent );
 			return;
 		} else if ( elt < nd->_elt )
 			_insert( nd->_left, elt, nd );
@@ -91,71 +119,188 @@ template < class T > class rbt {
 			_insert( nd->_right, elt, nd );
 		else
 			return;
-		switch ( nd->_change ) {
-			case RIGHT_ROT:
-				nd->_change = 0;
-				rightRotation( nd );
-				swapColors( nd, nd->_right );
-				return;
-			case LEFT_ROT:
-				nd->_change = 0;
-				leftRotation( nd );
-				swapColors( nd, nd->_left );
-				return;
-		}
-		node< T > *sibling = getSibling( nd );
-		if ( !nd->_parent || nd->_color == BLACK_NODE ||
-			 ( ( !nd->_left || nd->_left->_color == BLACK_NODE ) &&
-			   ( !nd->_right || nd->_right->_color == BLACK_NODE ) ) )
+		rotate( nd, true );
+		node *sibling = getSibling( nd );
+		if ( !nd->_parent || nd->_color == BLACK ||
+			 ( ( !nd->_left || nd->_left->_color == BLACK ) &&
+			   ( !nd->_right || nd->_right->_color == BLACK ) ) )
 			return;
-		else if ( !sibling || sibling->_color == BLACK_NODE ) {
-			if ( nd == nd->_parent->_left && elt < nd->_elt ) nd->_parent->_change = RIGHT_ROT;
+		else if ( !sibling || sibling->_color == BLACK ) {
+			if ( nd == nd->_parent->_left && elt < nd->_elt ) nd->_parent->_rotate = RIGHT;
 			else if ( nd == nd->_parent->_right && elt > nd->_elt )
-				nd->_parent->_change = LEFT_ROT;
+				nd->_parent->_rotate = LEFT;
 			else if ( nd == nd->_parent->_left && elt > nd->_elt ) {
 				leftRotation( nd );
-				nd->_parent->_change = RIGHT_ROT;
+				nd->_parent->_rotate = RIGHT;
 			} else {
 				rightRotation( nd );
-				nd->_parent->_change = LEFT_ROT;
+				nd->_parent->_rotate = LEFT;
 			}
 		} else {
-			nd->_color			= BLACK_NODE;
-			sibling->_color		= BLACK_NODE;
-			nd->_parent->_color = RED_NODE;
+			nd->_color			= BLACK;
+			sibling->_color		= BLACK;
+			nd->_parent->_color = RED;
 		}
 	};
-	void destruct( node< T > *&nd ) {
+	void fixDBRotate( node *&nd ) {
+		if ( !nd->_rotate ) return;
+		node *parent = nd->_parent;
+		int	  dir	 = 0;
+		if ( !parent )
+			;
+		else if ( nd == parent->_left )
+			dir = -1;
+		else
+			dir = 1;
+		rotate( nd, false );
+		if ( dir == -1 ) parent->_left = nd;
+		else if ( dir == 1 )
+			parent->_right = nd;
+		else
+			root = nd;
+	};
+	void fixDB( node *&nd ) {
+		fixDBRotate( nd );
+		if ( nd->_color != DOUBLE_BLACK && !nd->_nil ) return;
+		else if ( !nd->_parent )
+			nd->_color = BLACK;
+		else {
+			node *DBsibling = getDBSibling( nd );
+			int	  DBdir		= ( nd->_nil ? nd->_nil : ( nd == nd->_parent->_left ? LEFT : RIGHT ) );
+			if ( DBsibling->_color == BLACK ) {
+				if ( ( !DBsibling->_left && !DBsibling->_right ) ||
+					 ( DBsibling->_left && DBsibling->_left->_color == BLACK && DBsibling->_right &&
+					   DBsibling->_right->_color == BLACK ) ) {
+					DBsibling->_color--;
+					if ( nd->_nil ) {
+						nd->_color++;
+						nd->_nil = 0;
+						fixDB( nd );
+					} else {
+						nd->_color--;
+						nd->_parent->_color++;
+						fixDB( nd->_parent );
+					}
+				} else if ( ( DBdir == LEFT && DBsibling->_right &&
+							  DBsibling->_right->_color == RED ) ||
+							( DBdir == RIGHT && DBsibling->_left &&
+							  DBsibling->_left->_color == RED ) ) {
+					node *farRed = ( DBdir == LEFT ? DBsibling->_right : DBsibling->_left );
+					if ( nd->_nil ) {
+						int tmpColor	  = nd->_color;
+						nd->_color		  = DBsibling->_color;
+						DBsibling->_color = tmpColor;
+						nd->_rotate		  = DBdir;
+						nd->_nil		  = 0;
+						farRed->_color++;
+						fixDBRotate( nd );
+					} else {
+						std::cout << "here\n";
+						int tmpColor		 = nd->_parent->_color;
+						nd->_parent->_color	 = DBsibling->_color;
+						DBsibling->_color	 = tmpColor;
+						nd->_parent->_rotate = DBdir;
+						nd->_color--;
+						farRed->_color++;
+						fixDB( nd->_parent );
+					}
+				} else {
+					int tmpColor	  = DBsibling->_color;
+					DBsibling->_color = ( DBsibling->_left && DBsibling->_left->_color == RED ?
+											  DBsibling->_left->_color :
+											  DBsibling->_right->_color );
+					( DBsibling->_left && DBsibling->_left->_color == RED ?
+						  DBsibling->_left->_color :
+						  DBsibling->_right->_color ) = tmpColor;
+					DBsibling->_rotate				  = ( DBdir == LEFT ? RIGHT : LEFT );
+					fixDBRotate( DBsibling );
+					fixDB( nd );
+				}
+			} else {
+				if ( nd->_nil ) {
+					int tmpColor	  = nd->_color;
+					nd->_color		  = DBsibling->_color;
+					DBsibling->_color = tmpColor;
+					nd->_rotate		  = DBdir;
+					fixDBRotate( nd );
+				} else {
+					int tmpColor		 = nd->_parent->_color;
+					nd->_parent->_color	 = DBsibling->_color;
+					DBsibling->_color	 = tmpColor;
+					nd->_parent->_rotate = DBdir;
+					fixDB( nd->_parent );
+				}
+			}
+		}
+	};
+	void _del( node **nd, const T &elt ) {
+		if ( !*nd ) return;
+		else if ( elt < ( *nd )->_elt )
+			_del( &( *nd )->_left, elt );
+		else if ( elt > ( *nd )->_elt )
+			_del( &( *nd )->_right, elt );
+		else {
+			if ( !( *nd )->_left && !( *nd )->_right ) {
+				node *parent = ( *nd )->_parent;
+				if ( parent && ( *nd )->_color == BLACK )
+					parent->_nil = ( ( *nd ) == parent->_left ? LEFT : RIGHT );
+				delete *nd;
+				*nd = NULL;
+				fixDB( parent );
+			} else if ( !( *nd )->_left || !( *nd )->_right ) {
+				node *tmp	 = *nd;
+				node *sub	 = ( *nd )->_left ? ( *nd )->_left : ( *nd )->_right;
+				sub->_color	 = ( *nd )->_color;
+				sub->_parent = ( *nd )->_parent;
+				( *nd )		 = sub;
+				delete tmp;
+			} else {
+				node *sub = ( *nd )->_right;
+				while ( sub->_left ) sub = sub->_left;
+				( *nd )->_elt = sub->_elt;
+				_del( &( *nd )->_right, sub->_elt );
+			}
+		}
+		if ( *nd ) fixDB( *nd );
+	};
+	void destruct( node *&nd ) {
 		if ( nd->_left ) destruct( nd->_left );
 		if ( nd->_right ) destruct( nd->_right );
 		delete nd;
 		nd = NULL;
 	};
-	void _log( node< T > *x, const int lvl ) {
+	void _log( node *x, const int lvl ) {
 		if ( !x ) return;
 		_log( x->_right, lvl + 1 );
 		for ( int i = 0; i < lvl * 4; i++ ) std::cout << ' ';
-		std::cout << ( x->_color == BLACK_NODE ? "\e[1;37m" : "\e[1;31m" ) << x->_elt << "\e[0m\n";
+		std::cout << ( x->_color == RED ? "\e[1;31m" : "\e[1;37m" )
+				  << ( x->_color == DOUBLE_BLACK ? "\e[4m" : "" ) << x->_elt << "\e[0m\n";
 		_log( x->_left, lvl + 1 );
 	};
-	node< T > *root;
+	node *root;
 
   public:
 	rbt( void ) { root = NULL; };
-	rbt( const T &n ) { root = new node< T >( n ); };
-	~rbt( void ) { destruct( root ); };
+	rbt( const T &n ) { root = new node( n ); };
+	~rbt( void ) {
+		if ( root ) destruct( root );
+	};
 	void insert( const T &elt ) {
 		_insert( root, elt, NULL );
-		root->_color = BLACK_NODE;
+		root->_color = BLACK;
 	};
-	// bool find( const T &elt ) {
-	// 	if ( !root ) return false;
-	// 	else
-	// 		return find( *root, elt );
-	// };
-	// void del( const T &elt ) { _del( root, elt ); };
+	bool find( const T &elt ) {
+		if ( !root ) return false;
+		else
+			return find( *root, elt );
+	};
+	void del( const T &elt ) { _del( &root, elt ); };
+	//
+	//
+	// tmp
 	void log( void ) {
+		if ( !root ) return;
 		_log( root, 0 );
-		std::cout << "\n";
+		std::cout << "----------------------\n";
 	};
 };
